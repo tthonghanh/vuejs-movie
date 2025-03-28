206d5667723912e1b82ba2600e7ee3b4e5ade587
Tran Thi Hong Hanh <tthh260904@gmail.com>
2025-03-28 19:55:00 +0700

    delete sidebar and topbar

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,14 @@
 <script lang="ts">
 // import HeaderComponent from './components/HeaderComponent/HeaderComponent.vue';
 import { Vue, Component } from 'vue-property-decorator'
-import TopBarComponent from './components/TopBarComponent.vue';
 import LogOutModalComponent from './components/LogoutModalComponent.vue';
 import HomeComponent from './components/HomeComponent.vue';
-import SideBarComponent from './components/SideBarComponent.vue';
 
 @Component({
   components: {
     // HeaderComponent
-    TopBarComponent,
     LogOutModalComponent,
     HomeComponent,
-    SideBarComponent,
   }
 })
 class App extends Vue {
